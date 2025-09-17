45b6a8665dc912e1f8654971152238be53bf87eb
tarunpratap1 <tpwonderno1@gmail.com>
2025-09-17 13:30:04 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+import cors from "cors";
+app.use(cors({ origin: "*" })); // allow all origins
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -285,4 +289,5 @@ app.listen(PORT, () => console.log(`✅ Backend running at http://localhost:${PO
 // });
 
 // const PORT = process.env.PORT || 5000;
+
 // app.listen(PORT, () => console.log(`✅ Backend running at http://localhost:${PORT}`));
